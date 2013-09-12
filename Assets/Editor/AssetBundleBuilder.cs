ad0d5c020f93e732709df21c4904736012c1fa0e
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-12 18:18:37 +0900

    Added dependencies.

--- Assets/Editor/AssetBundleBuilder.cs
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -34,19 +34,30 @@ public class AssetBundleBuilder
             return;
         }
 
-        var prefab = AssetDatabase.LoadMainAssetAtPath ("Assets/Plane.prefab") as GameObject;
+		BuildPipeline.PushAssetDependencies ();
+
+		var shader = AssetDatabase.LoadMainAssetAtPath ("Assets/Aurora.shader");
+		var path = System.IO.Path.Combine (destination, "common.unity3d");
+		var options = BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets;
+		BuildPipeline.BuildAssetBundle (shader, new Object[] {shader}, path, options, CurrentPlatform);
+
+		var prefab = AssetDatabase.LoadMainAssetAtPath ("Assets/Plane.prefab") as GameObject;
 
         for (var i = 0; i < 9; i++) {
-            prefab.renderer.sharedMaterial.SetVector ("_UAnimR", RandomParameter ());
+			prefab.renderer.sharedMaterial.SetVector ("_UAnimR", RandomParameter ());
             prefab.renderer.sharedMaterial.SetVector ("_VAnimR", RandomParameter ());
             prefab.renderer.sharedMaterial.SetVector ("_UAnimG", RandomParameter ());
             prefab.renderer.sharedMaterial.SetVector ("_VAnimG", RandomParameter ());
             prefab.renderer.sharedMaterial.SetVector ("_UAnimB", RandomParameter ());
             prefab.renderer.sharedMaterial.SetVector ("_VAnimB", RandomParameter ());
 
-            var path = System.IO.Path.Combine (destination, "panel" + i + ".unity3d");
-            var options = BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets;
-            BuildPipeline.BuildAssetBundle (prefab, new Object[] {prefab}, path, options, CurrentPlatform);
+            path = System.IO.Path.Combine (destination, "panel" + i + ".unity3d");
+
+			BuildPipeline.PushAssetDependencies ();
+			BuildPipeline.BuildAssetBundle (prefab, new Object[] {prefab}, path, options, CurrentPlatform);
+			BuildPipeline.PopAssetDependencies();
         }
-    }
+
+		BuildPipeline.PopAssetDependencies();
+	}
 }
